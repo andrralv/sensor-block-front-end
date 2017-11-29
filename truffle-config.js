7594c1aa09bef12927664d3196a5a840307a26fc
Andrralv <andrralv@gmail.com>
2017-11-29 14:24:17 -0600

    appears to be working now

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,11 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      network_id: "*",
+      host: "127.0.0.1",
+      port: 8545,
+      gas: 500000,
+      gasPrice: 1000 // Specified in Wei
+    }
+  }
 };
