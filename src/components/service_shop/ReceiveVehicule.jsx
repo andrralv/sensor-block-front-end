8914a6e02d5d68ab29ba84adb4916a4aa9846f5b
ttymarucr <ttymarucr@gmail.com>
2017-12-08 08:04:29 -0600

    Ajustes al css del form
    El label para el textarea no se mueve cuando hay input de datos.

--- src/components/service_shop/ReceiveVehicule.jsx
+++ src/components/service_shop/ReceiveVehicule.jsx
@@ -1,12 +1,38 @@
 import React, { Component } from 'react'
+import Navigation from '../Navigation'
+import '../../css/form.css'
 
 export class ReceiveVehicule extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "Receive Vehicule"
+        };
+    }
+
     render() {
-        return ( 
-        <div className="navigation">
-            <h1>Receive Vehicule</h1>
-        </div>
+        return (
+            <div className="App">
+                <Navigation title={this.state.title} />
+                <div className="margin-left">
+                    <div className="container">
+                        <div id="blue-form">
+                            <div className="form-container">
+                                <input className="input-field" required autoFocus />
+                                <label className="input-label">Vin</label>
+                                <div className="input-subline"></div>
+                            </div>
+                            <i className="material-icons w3-xxlarge next">arrow_forward</i>
+                            <div className="form-container">
+                                <textarea className="input-field" cols="40" rows="5"/>
+                                <label className="input-label">Comments</label>
+                                <div className="input-subline"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 
